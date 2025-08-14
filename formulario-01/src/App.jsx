82aee62ad812daeed275a81d9405c8d82d998055
hviveros <humberto.viverosrios@gmail.com>
2025-08-13 22:50:33 -0300

    Validación de campos con React Hook Form

--- formulario-01/src/App.jsx
+++ formulario-01/src/App.jsx
@@ -1,39 +1,53 @@
-import { useRef } from "react";
+import { useForm } from 'react-hook-form'
 import "./App.css"
 
 const App = () => {
 
-    const data = { email: "", password:"" }
+    const { register, handleSubmit, watch, formState:{ errors } } = useForm()
 
-    // Componentes no controlados
-    const loginForm = useRef()
-
-    const submit = e => {
-        e.preventDefault()
-        console.log(loginForm.current)
-        alert("Welcome!")
+    const submit = data => {
+        console.log(data)
     }
+
+    // console.log(watch("email"))
+    console.log(errors)
     
     return(
         <div>
-            <form className="container" onSubmit={submit} ref={loginForm}>
+            <form className="container" onSubmit={handleSubmit(submit)} >
                 <div className="item form__item">
                     <label htmlFor="email">Correo electrónico</label>
                     <input 
+                        { ...register("email", { required: "El correo es obligatorio" }) }
                         type="email" 
                         name="email" 
                         id="email" 
-                        defaultValue={data.email}
+                        defaultValue=""
                     />
+                    { errors?.email && 
+                        <span className="color-red">
+                            { errors?.email?.message }
+                        </span> 
+                    }
                 </div>
                 <div className="item form__item">
                     <label htmlFor="password">Contraseña</label>
                     <input 
+                        { ...register("password", {
+                            required: "La contraseña es obligatoria",
+                            minLength: 4,
+                            maxLength: { value: 10, message: "No puede exceder 10 caracteres" }
+                        })}
                         type="password" 
                         name="password" 
                         id="password" 
-                        defaultValue={data.password}
+                        defaultValue=""
                     />
+                    { errors?.password && 
+                        <span className="color-red">
+                            { errors?.password?.message }
+                        </span> 
+                    }
                 </div>
                 <div className="item form__item">
                     <input type="submit" value="Ingresar" className="button full" />
